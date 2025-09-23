5a66a946e3529b9bdd85e447b40f3df8dc28d9a8
guscila <guscila@OKI>
2025-09-23 22:37:52 +0300

    programa papildyta generuojamais atsitiktiniais namu darbu ir egzamino balais bei aiskiu pasirinkimo meniu

--- v0.1/v0.1.cpp
+++ v0.1/v0.1.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <random>
 
 using std::cout;
 using std::cin;
@@ -16,8 +17,10 @@ using std::right;
 using std::setfill;
 using std::setprecision;
 using std::fixed;
-using std::string;
 using std::sort;
+using std::uniform_int_distribution;
+using std::mt19937;
+using std::random_device;
 
 struct Studentas {
     string vardas, pavarde;
@@ -44,24 +47,44 @@ int main()
 
 Studentas ivesk() {
     Studentas laik;
-    int sum = 0, n, paz;
+    int sum = 0, n, paz, x;
     string ivestis;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 10);
     cout << "Iveskite studento varda ir pavarde: ";
     cin >> laik.vardas >> laik.pavarde;
-    cout << "Iveskite studento namu darbu pazymius, kai noresite sustoti iveskite zodi 'baigta':" << endl;
-    while (true) {
-        cin >> ivestis;
-        if (ivestis == "baigta") break;
-        try {
-            paz = stoi(ivestis);
+    cout << "Jei norite, ivesti namu darbu pazymius ir egzamino bala rankiniu budu - iveskite 1, jei norite, kad pazymiai butu sugeneruoti atsitiktinai - iveskite 2: "; cin >> x;
+    while (x != 1 && x != 2) {
+        cout << "Neteisinga ivestis. ";
+        cout << "Jei norite, ivesti namu darbu pazymius ir egzamino bala rankiniu budu - iveskite 1, jei norite, kad pazymiai butu sugeneruoti atsitiktinai - iveskite 2: "; cin >> x;
+    }
+    if (x == 1) {
+        cout << "Iveskite studento namu darbu pazymius, kai noresite sustoti iveskite zodi 'baigta':" << endl;
+        while (true) {
+            cin >> ivestis;
+            if (ivestis == "baigta") break;
+            try {
+                paz = stoi(ivestis);
+                laik.pazymiai.push_back(paz);
+                sum += paz;
+            }
+            catch (...) {
+                cout << "Neteisinga ivestis - iveskite skaiciu, o jei norite baigti, iveskite zodi 'baigta':" << endl;
+            }
+        }
+        cout << "Iveskite egzamino bala: "; cin >> laik.egzas;
+    }
+    else if (x == 2) {
+        int nd;
+        cout << "Iveskite kiek studento namu darbu pazymiu norite atsitiktinai sugeneruoti: "; cin >> nd;
+        for (int i = 0; i < nd; i++) {
+            paz = dist(gen);
             laik.pazymiai.push_back(paz);
             sum += paz;
         }
-        catch (...) {
-            cout << "Neteisinga ivestis - iveskite skaiciu, o jei norite baigti, iveskite zodi 'baigta':" << endl;
-        }
+        laik.egzas = dist(gen);
     }
-    cout << "Iveskite egzamino bala: "; cin >> laik.egzas;
     laik.rez = laik.egzas * 0.6 + double(sum) / double(laik.pazymiai.size()) * 0.4;
     n = laik.pazymiai.size();
     sort(laik.pazymiai.begin(), laik.pazymiai.end());
@@ -76,7 +99,11 @@ Studentas ivesk() {
 
 void isvesk(const vector<Studentas>& grupe) {
     int x; 
-    cout << "Jei norite, kad programa isvestu tik studentu vidurkius - iveskite 1, jei norite, kad programa isvestu tik studentu medianas - iveskite 2, jei norite, kad isvestu abi reiksmes - iveskite 3: " << endl; cin >> x;
+    cout << "Jei norite, kad programa isvestu tik studentu vidurkius - iveskite 1, jei norite, kad programa isvestu tik studentu medianas - iveskite 2, jei norite, kad isvestu abi reiksmes - iveskite 3: "; cin >> x;
+    while (x != 1 && x != 2 && x != 3) {
+        cout << "Neteisinga ivestis. ";
+        cout << "Jei norite, kad programa isvestu tik studentu vidurkius - iveskite 1, jei norite, kad programa isvestu tik studentu medianas - iveskite 2, jei norite, kad isvestu abi reiksmes - iveskite 3: "; cin >> x;
+    }
     if (x == 1) {
         cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << endl;
         cout << string(50, '-') << endl;
