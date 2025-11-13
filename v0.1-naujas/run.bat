3dd6660757a2be542e48cad017b6f5e9d9796274
guscila <guscila@OKI>
2025-11-13 23:54:41 +0200

    pilnai sukurti CMakeLists.txt ir run.bat failai

--- v0.1-naujas/run.bat
+++ v0.1-naujas/run.bat
@@ -1,20 +1,7 @@
 @echo off
-setlocal
-set "BUILD=build"
-
-REM Configure (Release for single-config; MSVC uses --config below)
-cmake -S . -B "%BUILD%" -DCMAKE_BUILD_TYPE=Release || exit /b 1
-
-REM Build (Release)
-cmake --build "%BUILD%" --config Release || exit /b 1
-
-REM Run (works for MSVC and MinGW/Ninja layouts)
-set "EXE=%BUILD%\app.exe"
-if not exist "%EXE%" set "EXE=%BUILD%\Release\app.exe"
-
-if not exist "%EXE%" (
-  echo Could not find app.exe in "%BUILD%" or "%BUILD%\Release".
-  exit /b 1
-)
-
-"%EXE%" %*
+if not exist build mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release
+if exist .\v1.0.exe (.\v1.0.exe %*) else (.\Release\v1.0.exe %*)
+pause
